2b63dedd13c0ef9ab1233cf7a9943e4383120329
Seungjae Park <astro.psj@gmail.com>
2020-03-04 02:21:43 +0900

    Remove unexecutable code on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ int main() {
         .setLogLevel(trantor::Logger::kWarn)
         .addListener("0.0.0.0", 80) //TODO: loadConfigFile("./config.json")
         .setThreadNum(16)
-        .enableRunAsDaemon()
         .registerController(std::make_shared<libresim::api::v1::TimeHttpController>())
         .run();
 }
